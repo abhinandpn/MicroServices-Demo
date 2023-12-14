8e442d6fe2a23ad2388f9100d4342d306fcf9f49
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 21:52:26 +0530

    router package

--- Product-Service/pkg/api/routes/product.go
+++ Product-Service/pkg/api/routes/product.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/api/handler/interfaces"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRoute(api *gin.RouterGroup,
+	ProductHandler interfaces.ProductHandler) {
+
+}
