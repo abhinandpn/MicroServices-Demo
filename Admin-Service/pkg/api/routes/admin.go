3cb85d55152b78ba7a2be95081e11ebc2fbc173b
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 15:11:21 +0530

    router packeg

--- Admin-Service/pkg/api/routes/admin.go
+++ Admin-Service/pkg/api/routes/admin.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	handlerinterface "github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/api/handler/interfaces"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoute(api *gin.RouterGroup,
+	AdminHandler handlerinterface.Adminhandler) {
+
+}
