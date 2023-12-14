ab04df9935463c794f52846241944201b42d4399
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 21:49:44 +0530

    repository added

--- Product-Service/pkg/repository/product.go
+++ Product-Service/pkg/repository/product.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type ProductDatabse struct {
+	DB *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) interfaces.ProductRepository {
+	return ProductDatabse{DB: db}
+}
