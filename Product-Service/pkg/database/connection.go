fa8b407d3c919fc791b769688b278fd8e4f95b50
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 21:51:14 +0530

    connaction through databse

--- Product-Service/pkg/database/connection.go
+++ Product-Service/pkg/database/connection.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/config"
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
+
+	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+
+	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
+	err := db.AutoMigrate(
+		model.Proruct{},
+	)
+	// Error handling While Database Table creating
+	if err != nil {
+		return nil, err
+	}
+
+	return db, dbErr
+
+}
