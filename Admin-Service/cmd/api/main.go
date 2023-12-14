a98abb6c613f7e1ae35b7d0c69828da75ae44990
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 15:12:20 +0530

    min file and dependecies updating

--- Admin-Service/cmd/api/main.go
+++ Admin-Service/cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	config "github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/config"
+	di "github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/di"
+)
+
 func main() {
-	
+
+	cfg, cfgErr := config.LoadConfig()
+	if cfgErr != nil {
+		log.Fatal("cannot load config: ", cfgErr)
+	}
+
+	server, diErr := di.InitializeAPI(cfg)
+
+	if diErr != nil {
+		log.Fatal("cannot start server: ", diErr)
+	} else {
+		server.Run()
+	}
 }
