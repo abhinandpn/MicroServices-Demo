8ae6f103651bd5e7db2f6dfb3ff68319bfcf87f0
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 15:09:07 +0530

    repostiry package added

--- Admin-Service/pkg/repository/admin.go
+++ Admin-Service/pkg/repository/admin.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type AdminDatabase struct {
+	DB *gorm.DB
+}
+
+func NewAdminRepository(DB *gorm.DB) interfaces.AdminRepository {
+	return &AdminDatabase{DB: DB}
+}
