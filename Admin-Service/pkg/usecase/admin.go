beef2502983eed4b4580a68a9520acd95c9f7b67
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 15:08:48 +0530

    usecase file updated

--- Admin-Service/pkg/usecase/admin.go
+++ Admin-Service/pkg/usecase/admin.go
@@ -1,1 +1,15 @@
 package usecase
+
+import (
+	service "github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/usecase/interfaces"
+
+	"github.com/abhinandpn/MicroServices-Demo/Admin-Service/pkg/repository/interfaces"
+)
+
+type AdminUsecase struct {
+	AdminRepo interfaces.AdminRepository
+}
+
+func NewAdminUseCase(AdminRepo interfaces.AdminRepository) service.AdminUsecase {
+	return &AdminUsecase{AdminRepo: AdminRepo}
+}
--- Admin-Service/pkg/usecase/interfaces/adminInterface.go
+++ Admin-Service/pkg/usecase/interfaces/adminInterface.go
@@ -1,1 +1,5 @@
 package interfaces
+
+type AdminUsecase interface{
+	
+}
