dae69ded2bc65f460c06be127178937a244efd4e
abhinandpn <abhinandpn4067@gmail.com>
2023-12-14 21:52:47 +0530

    handler package for main func

--- Product-Service/pkg/api/handler/product.go
+++ Product-Service/pkg/api/handler/product.go
@@ -1,1 +1,14 @@
 package handler
+
+import (
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/api/handler/interfaces"
+	service "github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/usecase/interfaces"
+)
+
+type ProductHandler struct {
+	productusecase service.ProductUsecase
+}
+
+func NewProductHandler(ProductUserCase service.ProductUsecase) interfaces.ProductHandler {
+	return ProductHandler{productusecase: ProductUserCase}
+}
