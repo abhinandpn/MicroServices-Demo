4df52f739464d11de92664bba882d284f5318db8
abhinandpn <abhinandpn4067@gmail.com>
2023-12-15 12:40:29 +0530

    repository added

--- User-Service/pkg/repository/user.go
+++ User-Service/pkg/repository/user.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"github.com/abhinandpn/MicroServices-Demo/User-Service/pkg/repository/interfaces"
+	"gorm.io/gorm"
+)
+
+type UserDatabase struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(DB *gorm.DB) interfaces.UserReporitory {
+	return UserDatabase{DB: DB}
+}
