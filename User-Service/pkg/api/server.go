f473d3795106e5562922d4496023d5f503d21ec1
abhinandpn <abhinandpn4067@gmail.com>
2023-12-15 12:42:54 +0530

    main server file

--- User-Service/pkg/api/server.go
+++ User-Service/pkg/api/server.go
@@ -1,1 +1,35 @@
 package http
+
+import (
+	handlerinterface "github.com/abhinandpn/MicroServices-Demo/User-Service/pkg/api/handler/interfaces"
+	"github.com/abhinandpn/MicroServices-Demo/User-Service/pkg/api/routes"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type ServerHTTP struct {
+	engine *gin.Engine
+}
+
+func NewServerHTTP(
+userhandler handlerinterface.UserHandler,
+)*ServerHTTP{
+	Engine := gin.New()
+	Engine.Use(gin.Logger())
+
+	// For Swagger
+	Engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	//route
+	routes.UserRoute(Engine.Group("/user"),userhandler)
+
+	return &ServerHTTP{engine: Engine}
+	
+}
+
+func (sh *ServerHTTP) Start() {
+	sh.engine.Run(":5010")
+}
