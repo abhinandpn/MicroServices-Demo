fa5da32017bb7ec0a6e83714367f8b993c6cad78
abhinandpn <abhinandpn4067@gmail.com>
2023-12-19 14:53:06 +0530

    hadlder updating ....

--- Product-Service/pkg/api/handler/interfaces/handlerInterface.go
+++ Product-Service/pkg/api/handler/interfaces/handlerInterface.go
@@ -1,4 +1,11 @@
 package interfaces
 
+import "github.com/gin-gonic/gin"
+
 type ProductHandler interface {
+	AddProduct(ctx *gin.Context)
+	GetProductById(ctx *gin.Context)
+	GetProducts(ctx *gin.Context)
+	DeleteProduct(ctx *gin.Context)
+	UpdateProduct(ctx *gin.Context)
 }
--- Product-Service/pkg/api/handler/product.go
+++ Product-Service/pkg/api/handler/product.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/api/handler/interfaces"
 	service "github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/usecase/interfaces"
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/util/req"
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/util/res"
+	"github.com/gin-gonic/gin"
 )
 
 type ProductHandler struct {
@@ -10,5 +15,43 @@ type ProductHandler struct {
 }
 
 func NewProductHandler(ProductUserCase service.ProductUsecase) interfaces.ProductHandler {
-	return ProductHandler{productusecase: ProductUserCase}
+	return &ProductHandler{productusecase: ProductUserCase}
+}
+
+func (p *ProductHandler) AddProduct(ctx *gin.Context) {
+
+	var body req.ReqProduct
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		respones := res.ErrorResponse(400, "invalid input", err.Error(), body)
+		ctx.JSON(http.StatusBadRequest, respones)
+		return
+	}
+
+	err := p.productusecase.CreateProduct(body)
+	if err != nil {
+		response := res.ErrorResponse(400, "faild to add product", err.Error(), body)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := res.SuccessResponse(200, "successfully product added", body)
+	ctx.JSON(http.StatusOK, response)
+
+}
+
+func (p *ProductHandler) GetProductById(ctx *gin.Context) {
+
+}
+
+func (p *ProductHandler) GetProducts(ctx *gin.Context) {
+
+}
+
+func (p *ProductHandler) DeleteProduct(ctx *gin.Context) {
+
+}
+
+func (p *ProductHandler) UpdateProduct(ctx *gin.Context) {
+
 }
