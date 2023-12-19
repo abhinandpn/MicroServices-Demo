df60b11438142102be6c59e1cfe1a06c71b71baa
abhinandpn <abhinandpn4067@gmail.com>
2023-12-19 14:51:47 +0530

    updated table model for product

--- Product-Service/pkg/model/product.go
+++ Product-Service/pkg/model/product.go
@@ -1,9 +1,9 @@
 package model
 
 type Product struct {
-	Id          uint
-	ProductName string
-	Description string
-	Colour      string
-	Price       float64
+	Id          uint    `json:"id" gorm:"primaryKey;not null"`
+	ProductName string  `json:"product_name" gorm:"not null" binding:"required,min=3,max=50"`
+	Description string  `json:"description" gorm:"not null"`
+	Colour      string  `json:"colour" gorm:"not null" `
+	Price       float64 `json:"price" gorm:"not null"`
 }
