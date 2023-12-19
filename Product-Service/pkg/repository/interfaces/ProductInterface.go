b44e3f133c990a283f3a7c5992583dc8aeb25fcc
abhinandpn <abhinandpn4067@gmail.com>
2023-12-19 14:49:29 +0530

    product repo for > curd opdations

--- Product-Service/pkg/repository/interfaces/ProductInterface.go
+++ Product-Service/pkg/repository/interfaces/ProductInterface.go
@@ -1,9 +1,19 @@
 package interfaces
 
+import (
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/model"
+	"github.com/abhinandpn/MicroServices-Demo/Product-Service/pkg/util/req"
+)
+
 type ProductRepository interface {
-	// AddProduct
-	// GetProducts
-	// GetProductById
-	// DeleteProduct
-	// UpdateProduct
+
+	// CURD
+	AddProduct(product req.ReqProduct) error
+	GetProductById(Id uint) (model.Product, error)
+	DeleteProduct(Id uint) error
+	UpdateProduct(product req.ReqProduct, Id uint) error
+	GetProducts() ([]model.Product, error)
+
+	// Checking
+	GetProductByName(name string)(model.Product,error)
 }
