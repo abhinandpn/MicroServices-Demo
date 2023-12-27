b13e071afd298433267535dac851839d36ba9833
abhinandpn <abhinandpn4067@gmail.com>
2023-12-28 01:20:34 +0530

    updating packages and setuping

--- User-Service/cmd/api/main.go
+++ User-Service/cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/abhinandpn/MicroServices-Demo/User-Service/pkg/config"
+	"github.com/abhinandpn/MicroServices-Demo/User-Service/pkg/di"
+)
+
 func main() {
-	
+	cfg, configErr := config.LoadConfig()
+
+	if configErr != nil {
+		log.Fatal("cannot load config: ", configErr)
+	}
+
+	server, diErr := di.InitializeAPI(cfg)
+
+	if diErr != nil {
+		log.Fatal("cannot start server: ", diErr)
+	} else {
+		server.Start()
+	}
 }
