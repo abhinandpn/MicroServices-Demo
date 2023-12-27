ed8b48f274d82e3cc6822bc24145fb113536c34e
abhinandpn <abhinandpn4067@gmail.com>
2023-12-28 01:18:42 +0530

    database models

--- User-Service/pkg/model/user.go
+++ User-Service/pkg/model/user.go
@@ -1,5 +1,9 @@
 package model
 
-type User struct{
-	
+type User struct {
+	Id       uint   `json:"id" gorm:"not null" binding:"required"`
+	Name     string `json:"name" gorm:"not null" binding:"required"`
+	Email    string `json:"email" gorm:"not null" binding:"required"`
+	Phone    string `json:"phone" gorm:"not null" binding:"required"`
+	Password string `json:"password" gorm:"not null" binding:"required"`
 }
